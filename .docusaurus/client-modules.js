ec94005738324fa6ea60f52dc8dc5c0631d5fde9
custprop <custodianprop@gmail.com>
2024-01-15 22:05:45 +1100

    include all node_modules and .docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/rsn/Lafa.Codes/DevWorld/Projects/Docusaurus/custodianprop-web/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/rsn/Lafa.Codes/DevWorld/Projects/Docusaurus/custodianprop-web/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/rsn/Lafa.Codes/DevWorld/Projects/Docusaurus/custodianprop-web/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/rsn/Lafa.Codes/DevWorld/Projects/Docusaurus/custodianprop-web/src/css/custom.css"),
+];
